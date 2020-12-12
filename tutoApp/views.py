bc39127850bdb65da0bb7b261c414fa61503a22a
alainthierry <allspitfire19@gmail>
2020-12-12 18:54:10 +0100

    Use of a hmtl template -Configuration of a new installed app tutoApp -Definition of index view and call of exemple-html.html file

--- tutoApp/views.py
+++ tutoApp/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tutoApp.")
-    #return render(request, 'exemple-html.html')
+	return render(request, 'exemple-html.html')
